bda9cd8dfecc446a44eaf58a786b080333b38c0e
Preetham Sridhar <preethams+Zeta@zeta.tech>
2026-06-07 20:58:29 +0530

    add debouncing

--- cursor-tracker/src/components/cursortracker/index.js
+++ cursor-tracker/src/components/cursortracker/index.js
@@ -1,50 +1,48 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./styles.css";
 
-export const Cursortracker = ({height, width, render}) => {
+export const Cursortracker = ({ height, width, render }) => {
+  const [cursorPos, setCursorPos] = useState({});
+  const timeOutIdRef = useRef(null);
+  const onMouseTrack = (e) => {
+    timeOutIdRef.current = setTimeout(() => {
+      setCursorPos({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    }, 100);
 
-    const [cursorPos, setCursorPos] = useState({});
-    const onMouseTrack = (e) => {
-        
-        setCursorPos({
-            x: e.clientX,
-            y: e.clientY
-        })
-        console.log(`🚀 ~ onMouseTrack ~ {
-            x: e.clientX,
-            y: e.clientY
-        }:`, {
-            x: e.clientX,
-            y: e.clientY
-        })
+    return () => {
+        clearTimeout(timeOutIdRef.current);
+        timeOutIdRef.current = null;
     }
-    return (
-        <div className="cursor_tracker__container"> 
-            Cursor Tracker
-            <div
-                style={{
-                    width: `${width}px`,
-                    height: `${height}px`,
-                    border: "4px solid black",
-                    background: "white"
-                }}
-                onMouseMove={(e) => onMouseTrack(e)}
-            
-            > 
-                <div
-                    style={{
-                        position: "absolute",
-                        top: `${cursorPos.y}px`,
-                        left: `${cursorPos.x}px`,
-                        // backgroundColor: "blue",
-                        // height: "10px",
-                        // width: "10px",
-                        // borderRadius: "50%",
-                    }}
-                >
-                    {render}
-                </div>
-            </div>
+  };
+  return (
+    <div className="cursor_tracker__container">
+      Cursor Tracker
+      <div
+        style={{
+          width: `${width}px`,
+          height: `${height}px`,
+          border: "4px solid black",
+          background: "white",
+        }}
+        onMouseMove={(e) => onMouseTrack(e)}
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: `${cursorPos.y}px`,
+            left: `${cursorPos.x}px`,
+            // backgroundColor: "blue",
+            // height: "10px",
+            // width: "10px",
+            // borderRadius: "50%",
+          }}
+        >
+          {render}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
